4263e1d154a8ece4bf753d852411bbc770caad4d
omarpr1032 <omarpr1032@gmail.com>
2018-03-20 10:37:27 -0400

    Add files via upload

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -2,20 +2,23 @@ import webbrowser
 import os
 import re
 
+
 # Styles and scripting for the page
 main_page_head = '''
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="utf-8">
     <title>Fresh Tomatoes!</title>
 
     <!-- Bootstrap 3 -->
-    <link rel="stylesheet" href="https://netdna.bootstrapcdn.''' + \
-    '''com/bootstrap/3.1.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://netdna.bootstrapcdn.''' + \
-    '''com/bootstrap/3.1.0/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/''' + \
+    '''bootstrap/3.1.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/''' + \
+    '''bootstrap/3.1.0/css/bootstrap-theme.min.css">
     <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
-    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/ /
-     bootstrap.min.js"></script>
+    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/''' + \
+    '''js/bootstrap.min.js"></script>
     <style type="text/css" media="screen">
         body {
             padding-top: 80px;
@@ -60,19 +63,19 @@ main_page_head = '''
     <script type="text/javascript" charset="utf-8">
         // Pause the video when the modal is closed
         $(document).on('click', '.hanging-close, .modal-backdrop,''' + \
-    '''.modal', function (event) {
-            // Remove the src so the player itself gets removed, ''' + \
-    '''as this is the only
+    ''' .modal', function (event) {
+            // Remove the src so the player itself gets removed,''' + \
+    ''' as this is the only
             // reliable way to ensure the video stops playing in IE
             $("#trailer-video-container").empty();
         });
         // Start playing the video whenever the trailer modal is opened
         $(document).on('click', '.movie-tile', function (event) {
             var trailerYouTubeId = $(this).attr('data-trailer-youtube-id')
-            var sourceUrl = 'http://www.youtube.com/embed/' + ''' + \
-    '''trailerYouTubeId + '?autoplay=1&html5=1';
-            $("#trailer-video-container").empty().append($("<iframe>''' + \
-    '''</iframe>", {
+            var sourceUrl = 'http://www.youtube.com/embed/''' + \
+    '''' + trailerYouTubeId + '?autoplay=1&html5=1';
+            $("#trailer-video-container").empty().append(''' + \
+    '''$("<iframe></iframe>", {
               'id': 'trailer-video',
               'type': 'text-html',
               'src': sourceUrl,
@@ -89,19 +92,19 @@ main_page_head = '''
 </head>
 '''
 
+
 # The main page layout and title bar
 main_page_content = '''
-<!DOCTYPE html>
-<html lang="en">
   <body>
     <!-- Trailer Video Modal -->
     <div class="modal" id="trailer">
       <div class="modal-dialog">
         <div class="modal-content">
-          <a href="#" class="hanging-close" data-dismiss="modal"''' + \
-    ''' aria-hidden="true">
-            <img src="https://lh5.ggpht.com/v4-628SilF0HtHuHdu5EzxD7WR''' + \
-    '''qOrrTIDi_MhEG6_qkNtUK5Wg7KPkofp_VJoF7RS2LhxwEFCO1ICHZlc-o_=s0#w=24&h=24"/>
+          <a href="#" class="hanging-close" data-dismiss=''' + \
+    '''"modal" aria-hidden="true">
+            <img src="https://lh5.ggpht.com/v4-628SilF0Ht''' + \
+    '''HuHdu5EzxD7WRqOrrTIDi_MhEG6_qkNtUK5Wg7KPkofp_VJoF7''' + \
+    '''RS2LhxwEFCO1ICHZlc-o_=s0#w=24&h=24"/>
           </a>
           <div class="scale-media" id="trailer-video-container">
           </div>
@@ -126,6 +129,7 @@ main_page_content = '''
 </html>
 '''
 
+
 # A single movie entry html template
 movie_tile_content = '''
 <div class="col-md-6 col-lg-4 movie-tile text-center" ''' + \
@@ -146,8 +150,8 @@ def create_movie_tiles_content(movies):
             r'(?<=v=)[^&#]+', movie.trailer_youtube_url)
         youtube_id_match = youtube_id_match or re.search(
             r'(?<=be/)[^&#]+', movie.trailer_youtube_url)
-        trailer_youtube_id = youtube_id_match.group(
-            0) if youtube_id_match else None
+        trailer_youtube_id = (youtube_id_match.group(0) if youtube_id_match
+                              else None)
 
         # Append the tile for the movie with its content filled in
         content += movie_tile_content.format(
@@ -162,16 +166,15 @@ def open_movies_page(movies):
     # Create or overwrite the output file
     output_file = open('fresh_tomatoes.html', 'w')
 
-    # Replace the placeholder for the movie tiles with the actual dynamically
-    # generated content
+    # Replace the movie tiles placeholder generated content
     rendered_content = main_page_content.format(
-                      movie_tiles=create_movie_tiles_content(movies))
+        movie_tiles=create_movie_tiles_content(movies))
 
     # Output the file
     output_file.write(main_page_head + rendered_content)
     output_file.close()
 
-    # open the output file in the browser
+    # open the output file in the browser (in a new tab, if possible)
     url = os.path.abspath(output_file.name)
-    webbrowser.open('file://' + url, new=2)  # open in a new tab, if possible
+    webbrowser.open('file://' + url, new=2)
 
