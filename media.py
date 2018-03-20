1d4ddcade343e81f30783ca0175144d492efde47
omarpr1032 <omarpr1032@gmail.com>
2018-03-20 09:54:45 -0400

    Add files via upload

--- media.py
+++ media.py
@@ -1,20 +1,18 @@
 # Importing the webbrowser module to open URLs in interactive browser
 import webbrowser
 
-
 # Movie class definition
 class Movie():
-    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+    VALID_RATINGS = ["G","PG","PG-13","R"]
 
     # Cnstructor defining the instance variables
-    def __init__(self, movie_title, movie_storyline, poster_image,
-                 trailer_youtube):
-        self.title = movie_title
-        self.storyline = movie_storyline
-        self.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer_youtube
+    def __init__(self,movie_title,movie_storyline,poster_image,trailer_youtube):
+        self.title=movie_title
+        self.storyline=movie_storyline
+        self.poster_image_url=poster_image
+        self.trailer_youtube_url=trailer_youtube
 
     # Instance method to open the url on the web browser
     def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
-
+        webbrowser.open(self.trailer_youtube_url);
+        
